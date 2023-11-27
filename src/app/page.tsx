18a743ff0355532513b8a07b3c2d4da92789f6fa
Vrooty <victoriadohertybone@live.co.uk>
2023-11-27 10:09:14 +0000

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
-    <p>You are looking at my webpage. This must mean you're super cool.</p>
+    <p>You are looking at my webpage. This must mean you&apos;re super cool.</p>
     
     </main>  
   )
